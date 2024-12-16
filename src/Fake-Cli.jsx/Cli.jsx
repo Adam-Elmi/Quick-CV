0239534cfa9ce5e20da08e06ca09fd61775f2e6c
Adam Elmi <Adamcade123@gmail.com>
2024-12-16 22:58:36 +0300

    Made Some Changes

--- src/Fake-Cli.jsx/Cli.jsx
+++ src/Fake-Cli.jsx/Cli.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import SvgBook from "../Svg-Components/Svg-Book";
+import SvgPlay from "../Svg-Components/Svg-Play";
 
 export default function Cli() {
   return (
@@ -18,32 +19,58 @@ function CliHeader() {
       <h1 className="font-mono mobile:text-[0.85rem] small-mobile:text-[0.7rem]">
         C:Machine/SystemX/fake-cli.exe
       </h1>
-      <SvgBook />
+      <div className="flex gap-2">
+        <SvgPlay />
+        <SvgBook />
+      </div>
     </div>
   );
 }
 
 function CliBody() {
-    const handleKey = (e) => {
-        if (e.key === "Enter") {
-          e.preventDefault(); // Stops the default behavior
-          console.log("Enter key is pressed, but default action is prevented!");
-        }
-      };
-      
   return (
     <div className="flex-1 bg-[#0d1e41] h-full min-h-[500px] text-white p-2 px-3">
-      <div className="flex gap-4">
-        <p className="font-mono text-yellow-500 mobile:text-[0.85rem] small-mobile:text-[0.7rem]">
-          C:\Users\You\Quick-CV
-        </p>
-        <div
-          onKeyDown={handleKey}
-          contentEditable={true}
-          spellCheck={false}
-          className="font-mono text-slate-400 mobile:text-[0.85rem] small-mobile:text-[0.7rem] border-none outline-none bg-transparent overflow-hidden flex-1"
-        ></div>
-      </div>
+      <CliInput />
+    </div>
+  );
+}
+
+function CliInput() {
+  const userInputRef = useRef(null);
+
+  useEffect(() => {
+    const userInput = userInputRef.current;
+
+    userInput.focus();
+
+    const handleBlur = () => {
+      setTimeout(() => {
+        if (document.activeElement !== userInput) {
+          userInput.focus(); // Keep caret in input
+        }
+      }, 0);
+    };
+
+    // Add the blur event listener
+    userInput.addEventListener("blur", handleBlur);
+
+    // Cleanup on unmount
+    return () => {
+      userInput.removeEventListener("blur", handleBlur);
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-1">
+      <p className="font-mono text-yellow-500 mobile:text-[0.85rem] small-mobile:text-[0.55rem]">
+        C:\Users\You\Quick-CV
+      </p>
+      <input
+        ref={userInputRef}
+        id="user-input"
+        type="text"
+        className="font-mono text-slate-400 mobile:text-[0.85rem] small-mobile:text-[0.55rem] border-none outline-none bg-transparent overflow-hidden flex-1"
+      />
     </div>
   );
 }
