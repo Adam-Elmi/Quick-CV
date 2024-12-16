564d6878168f553ca6a51e59127a1d5aa08a13af
Adam Elmi <Adamcade123@gmail.com>
2024-12-16 16:07:51 +0300

    Made Some Changes

--- src/Site-Components/Steps-Section.jsx
+++ src/Site-Components/Steps-Section.jsx
@@ -8,7 +8,7 @@ export default function StepsSection() {
       {/* Left Section */}
       <div className="bg-white p-2 flex-1 flex justify-center items-center gap-4 flex-col w-[300px] max-w-full tablet:w-full">
         <p className="text-[1.5em] mobile:text-[1.1rem] small-mobile:text-[0.85rem] text-center text-slate-400 font-bold border-2 border-dotted p-2 mt-3 border-blue-100 rounded-full">Create and download a CV in 3 easy steps</p>
-        <img src={teamImage} alt="Picture"  className="max-w-full object-cover w-[600px]"/>
+        <img src={teamImage} alt="Picture"  className="max-w-full object-cover tablet:w-full scale-110 px-4 tablet:transform-none tablet:scale-100 tablet:px-0"/>
       </div>
       {/* Right Section */}
       <StepsComponent/>
@@ -19,9 +19,9 @@ export default function StepsSection() {
 function StepsComponent() {
   return (
     // Side Section Container
-    <div className="flex flex-col gap-5 p-2 flex-1 justify-end items-center">
+    <div className="flex flex-col gap-5 p-2 flex-1 justify-end items-center pt-10">
       {steps.map((value, id) => (
-        <div key={id} className="flex justify-between items-center shadow-lg border-2 max-w-[400px] w-full min-w-[300px] p-4 rounded-lg">
+        <div key={id} className="flex justify-between items-center shadow-lg border-2 max-w-[400px] w-full p-4 rounded-lg">
             <div className="flex flex-col gap-2">
                 <span className="text-slate-500 text-[0.8rem]">{value.step}</span>
                 <h2 className="text-[1.2rem] font-bold">{value.toDo}</h2>
