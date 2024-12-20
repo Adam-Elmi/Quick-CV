40b8ec704970cc360c292202e54d6462ede70920
Adam-Elmi <Adamcade123@gmail.com>
2024-12-20 22:24:36 +0300

    Modified Svg-Close.jsx

--- src/Svg-Components/Svg-Close.jsx
+++ src/Svg-Components/Svg-Close.jsx
@@ -1,22 +1,22 @@
 export default function SvgClose() {
-    return (
-      // Close/Cancle Icon (SVG)
-        <svg
-        width={24}
-        height={24}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <path
-          d="M6 1v3h1V2h11v11h-2v1h3V1H6zM1 6v13h13V6H1zm1 1h11v11H2V7zm2.879 2.172-.707.707L6.793 12.5l-2.621 2.621.707.707L7.5 13.207l2.621 2.621.707-.707L8.207 12.5l2.621-2.621-.707-.707L7.5 11.793 4.879 9.172z"
-          style={{
-            fill: "red",
-            fillOpacity: 1,
-            stroke: "none",
-            strokeWidth: 0,
-          }}
-        />
-      </svg>
-    )
-};
+  return (
+    // Close/Cancle Icon (SVG)
+    <svg
+      width={24}
+      height={24}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+    >
+      <path
+        d="M6 1v3h1V2h11v11h-2v1h3V1H6zM1 6v13h13V6H1zm1 1h11v11H2V7zm2.879 2.172-.707.707L6.793 12.5l-2.621 2.621.707.707L7.5 13.207l2.621 2.621.707-.707L8.207 12.5l2.621-2.621-.707-.707L7.5 11.793 4.879 9.172z"
+        style={{
+          fill: "red",
+          fillOpacity: 1,
+          stroke: "none",
+          strokeWidth: 0,
+        }}
+      />
+    </svg>
+  );
+}
 // Used in Sidebar component
