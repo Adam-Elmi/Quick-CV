fc8eaa6dfcf19fac32cb6c5f4b8f7bb9d2e93ab9
Adam-Elmi <Adamcade123@gmail.com>
2024-12-20 22:14:41 +0300

    Modified Resume.jsx

--- src/Resume-Components/Resume.jsx
+++ src/Resume-Components/Resume.jsx
@@ -43,47 +43,47 @@ export default function Resume() {
   }, []);
 
   return (
-      <div className=" w-full flex flex-col justify-center items-center p-5">
-        <h1 id="hide" className="my-5 font-bold text-[2rem] w-full text-center">
-          Create Your CV
-        </h1>
-        <div id="hide" className="flex gap-10  my-4">
-          <button
-            id="function-btn"
-            className="edit function-btn border-[1.5px] border-slate-300 rounded-md bg-white text-black hover:bg-slate-800 hover:text-white px-4 py-2 flex gap-2 justify-center items-center"
-          >
-            <span className="fa-solid fa-pen-to-square"></span>
-            <span className=" font-semibold rounded-md">Edit CV</span>
-          </button>
-          <button
-            id="function-btn"
-            className="preview function-btn border-[1.5px] border-slate-300 rounded-md bg-white text-black hover:bg-slate-800 hover:text-white px-4 py-2 flex gap-2 justify-center items-center"
-          >
-            <span className="fa-regular fa-eye"></span>
-            <span className="font-semibold rounded-md">Preview CV</span>
-          </button>
-        </div>
-        {isPreview ? (
-          <BasicTemplate />
-        ) : (
-          <>
-            {/* Input Form */}
-            <div className="max-w-[600px] w-full flex flex-col justify-center items-center gap-5 shadow-md px-2 mt-5 py-5 border-2 border-blue-500 rounded-md">
-              <Workflow
-                children={[
-                  <Contact key="contact" />,
-                  <Objective key="objective" />,
-                  <Skills key="skills" />,
-                  <Education key="education" />,
-                  <Work key="work" />,
-                  <Marital key="marital" />,
-                  <Certifications key="certifications" />,
-                  <GenerateCV key="generateCV" />,
-                ]}
-              />
-            </div>
-          </>
-        )}
+    <div className=" w-full flex flex-col justify-center items-center p-5">
+      <h1 id="hide" className="my-5 font-bold text-[2rem] w-full text-center">
+        Create Your CV
+      </h1>
+      <div id="hide" className="flex gap-10  my-4">
+        <button
+          id="function-btn"
+          className="edit function-btn border-[1.5px] border-slate-300 rounded-md bg-white text-black hover:bg-slate-800 hover:text-white px-4 py-2 flex gap-2 justify-center items-center"
+        >
+          <span className="fa-solid fa-pen-to-square"></span>
+          <span className=" font-semibold rounded-md">Edit CV</span>
+        </button>
+        <button
+          id="function-btn"
+          className="preview function-btn border-[1.5px] border-slate-300 rounded-md bg-white text-black hover:bg-slate-800 hover:text-white px-4 py-2 flex gap-2 justify-center items-center"
+        >
+          <span className="fa-regular fa-eye"></span>
+          <span className="font-semibold rounded-md">Preview CV</span>
+        </button>
       </div>
+      {isPreview ? (
+        <BasicTemplate />
+      ) : (
+        <>
+          {/* Input Form */}
+          <div className="max-w-[600px] w-full flex flex-col justify-center items-center gap-5 shadow-md px-2 mt-5 py-5 border-2 border-blue-500 rounded-md">
+            <Workflow
+              children={[
+                <Contact key="contact" />,
+                <Objective key="objective" />,
+                <Skills key="skills" />,
+                <Education key="education" />,
+                <Work key="work" />,
+                <Marital key="marital" />,
+                <Certifications key="certifications" />,
+                <GenerateCV key="generateCV" />,
+              ]}
+            />
+          </div>
+        </>
+      )}
+    </div>
   );
 }
