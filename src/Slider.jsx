1e46bd387902b37be1c7e2953d1bd62723e3ff3b
Adam-Elmi <Adamcade123@gmail.com>
2024-12-20 22:15:29 +0300

    Modified Slider.jsx

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,9 +1,9 @@
 export default function Slider() {
-    return (
-        <div className="w-full">
-            {/* Cv */}
-            {/* Arrows */}
-            {/* Current indicator */}
-        </div>
-    )
+  return (
+    <div className="w-full">
+      {/* Cv */}
+      {/* Arrows */}
+      {/* Current indicator */}
+    </div>
+  );
 }
