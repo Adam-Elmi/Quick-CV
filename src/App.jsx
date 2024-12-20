4df596170d04200a4108e0da168918325b27e399
Adam-Elmi <Adamcade123@gmail.com>
2024-12-20 21:39:39 +0300

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import Resume from "./Resume-Components/Resume";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Site-Components/Home";
-import Terminal from "./Fake-Terminal.jsx/Terminal";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cv" element={<Resume />} />
-      <Route path="/terminal" element={<Terminal/>} />
     </Routes>
   );
 }
