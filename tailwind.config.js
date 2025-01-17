4f00b8f6430aa2fcd243d9f0101161cbc6e4b949
Adam-Elmi <Adamcade123@gmail.com>
2025-01-17 14:37:31 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,10 @@ export default {
       screens: {
         "max-md" : {max : "768px"},
         "desktop": { max: "1024px" },
+        "max-lg": { max: "990px" },
         "tablet": { max: "768px" },
         "mobile": { max: "640px" },
+        "medium-mobile": { max: "480px" },
         "small-mobile": { max: "368px" },
       }
     },
