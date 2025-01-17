77ce82dcd29346738c351ca18fb9b91e12b1e6be
Adam-Elmi <Adamcade123@gmail.com>
2025-01-17 14:37:16 +0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Resume from "./Resume-Components/Resume";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Site-Components/Home";
+import Layout from "./Site-Components/Layout";
+import About from "./Site-Components/About";
+import ContactUs from "./Site-Components/ContactUs";
+import Contributors from "./Site-Components/contributors";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/contributors" element={<Contributors />} />
+      </Route>
       <Route path="/cv" element={<Resume />} />
     </Routes>
   );
