ea311a79c92433684fe5fd5c1ace80a86910e665
Adam-Elmi <Adamcade123@gmail.com>
2025-01-27 11:17:03 +0300

    Updated Core-Template.jsx

--- src/Resume-Components/Core-Template.jsx
+++ src/Resume-Components/Core-Template.jsx
@@ -1,40 +1,9 @@
-import { useEffect, useState } from "react";
+
 
 export function CoreTemplate() {
-    const [templateData, setTemplateData] = useState(null);
-    const [templatePath, setTemplatePath] = useState('/Templates/core-template/core-template.json');
-    useEffect(() => {
-      fetch(templatePath)
-        .then((res) => res.json())
-        .then((data) => {
-          setTemplateData(data);
-  
-          // Load CSS dynamically
-          const link = document.createElement("link");
-          link.rel = "stylesheet";
-          link.href = `/Templates/core-template/core-template.css`;
-          document.head.appendChild(link);
-  
-          return () => {
-            document.head.removeChild(link); // Clean up
-          };
-        });
-    }, [templatePath]);
-  
-    if (!templateData) return <p>Loading...</p>;
-  
-    return (
-      <div className="template-container">
-        {Object.entries(templateData.fields).map(([section, fields], idx) => (
-          <div key={idx} className="template-section">
-            <h3>{section}</h3>
-            {Object.entries(fields).map(([field, value], idx) => (
-              <p key={idx} className={`${templatePath.slice(templatePath.indexOf("/Templates/"), )} field-${field}`}>
-                {value || `Enter ${field}`}
-              </p>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="flex-1 border-2 border-red-500 h-[300px] p-2">
+      <h1>Hello Adam</h1>
+    </div>
+  )    
+}
