cad9e76b8e210d63aa2921d2b1d0b66a6ec72b40
Adam-Elmi <Adamcade123@gmail.com>
2025-02-02 23:52:57 +0300

    Updated CV component

--- src/Cv-Components/CV.jsx
+++ src/Cv-Components/CV.jsx
@@ -3,29 +3,38 @@ import GenerateCV from "../Helper-Components/GenerateCv";
 import Template from "./Template";
 
 export default function CV() {
-
   return (
-    <div className=" w-full flex flex-col justify-center items-center p-5">
-      <h1 id="hide" className="my-5 font-bold text-[2rem] w-full text-center">
-        Create Your CV
-      </h1>
-        <div className="w-full flex gap-5 justify-between items-center">
-          {/* Input Form */}
-          <div className="max-w-[600px] flex-1 flex flex-col justify-center items-center gap-5 shadow-md px-2 mt-5 py-5 border-2 border-blue-500 rounded-md">
-            <Workflow
-              children={[
-                "Personal Information",
-                "Education",
-                "Experience",
-                "Skills",
-                "Projects",
-                "Languages",
-                "Hobbies",
-              ]}
-            />
-          </div>
-          <Template />
+    <div className=" w-full flex flex-col justify-center items-center">
+      <div className="w-full min-h-[70px] bg-slate-900 flex items-center px-2">
+        <button className="text-white flex gap-2 justify-center items-center border-2 border-slate-600 p-2 rounded-md">
+          <span className="fa-solid fa-arrow-left text-slate-700"></span>
+          <span className="font-[500]">Templates</span>
+        </button>
+        <div className="flex-1 text-white text-center">
+          <input type="text" className="p-2 max-w-full w-[300px] text-center focus:text-left text-slate-300 bg-transparent outline-none focus:border-b-[1.5px] border-slate-700" placeholder="Enter the name of your CV (e.g., My-CV)." />
         </div>
+        <button className="text-white bg-indigo-600 flex gap-2 justify-center items-center border-2 border-slate-700 p-2 rounded-md">
+          <span className="font-[500]">Download</span>
+          <span className="fa-solid fa-download"></span>
+        </button>
+      </div>
+      <div className="w-full flex">
+        {/* Input Form */}
+        <div className="flex-1 min-h-screen flex gap-5 shadow-md">
+          <Workflow
+            children={[
+              "Personal Information",
+              "Education",
+              "Experience",
+              "Skills",
+              "Projects",
+              "Languages",
+              "Hobbies",
+            ]}
+          />
+        </div>
+        <Template />
+      </div>
     </div>
   );
 }
