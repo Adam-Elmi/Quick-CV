6614e30ce3801f58a86501b82c39802040c099be
Adam-Elmi <Adamcade123@gmail.com>
2025-02-03 22:03:15 +0300

    Updated Experience section

--- src/Sections/Experience.jsx
+++ src/Sections/Experience.jsx
@@ -1,30 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function Experience({ experience, defaultValue }) {
+export default function Eduction({ education, defaultValue }) {
   const [values, setValues] = useState(defaultValue);
-
   useEffect(() => {
     setValues(defaultValue);
   }, [defaultValue]);
   return (
-    <div id={experience.id}>
-      <div id="experience-title">{experience.title}</div>
-      <div id="experience-wrapper">
+    <div id={education.id}>
+      <div id="education-title">{education.title}</div>
+      <div id="education-wrapper">
         {values && Array.isArray(values)
           ? values.map((v, i) => (
               <div key={i}>
-                <div id={"role-" + (i + 1) + "wrapper"}>
-                  <p id={"role-" + (i + 1)}>{v.role}</p>
-                </div>
-                <div id={"company-" + (i + 1) + "wrapper"}>
-                  <p id={"company-" + (i + 1)}>{v.company}</p>
-                </div>
-                <div id={"ex-date-" + (i + 1) + "wrapper"}>
-                  <p id={"ex-date-" + (i + 1)}>{v.duration}</p>
-                </div>
-                <div id={"ex-description-" + (i + 1) + "wrapper"}>
-                  <p id={"ex-description-" + (i + 1)}>{v.description}</p>
-                </div>
+                <ul id={"institution-" + (i + 1) + "wrapper"}>
+                  <li id={"institution-" + (i + 1)}>{v.institution}</li>
+                </ul>
+                <ul id={"ed-year-" + (i + 1) + "wrapper"}>
+                  <li id={"ed-year-" + (i + 1)}>{v.year}</li>
+                </ul>
+                <ul id={"degree-" + (i + 1) + "wrapper"}>
+                  <li id={"degree-" + (i + 1)}>{v.degree}</li>
+                </ul>
+                <ul id={"ed-details-" + (i + 1) + "wrapper"}>
+                  <li id={"ed-details-" + (i + 1)}>{v.details}</li>
+                </ul>
               </div>
             ))
           : ""}
