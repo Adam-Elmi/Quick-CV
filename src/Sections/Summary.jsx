ad9652922cf53db78009df3f0db5813ad2638df3
Adam-Elmi <Adamcade123@gmail.com>
2025-02-03 22:03:20 +0300

    Updated Summary section

--- src/Sections/Summary.jsx
+++ src/Sections/Summary.jsx
@@ -1,9 +1,15 @@
-export default function Summary({ summary }) {
+import { useState, useEffect } from "react";
+
+export default function Summary({ summary, defaultValue }) {
+  const [value, setValue] = useState(defaultValue);
+    useEffect(() => {
+      setValue(defaultValue);
+    }, [defaultValue]);
   return (
     <div id={summary.id}>
       <div id="summary-title">{summary.title}</div>
       <div id="summary-text">
-        {summary.placeholder ? summary.placeholder : ""}
+        {value && Array.isArray(value) ? value[0] : ""}
       </div>
     </div>
   );
