95a6fbc7501ad61894b3c6e8cab82a31683b9cbe
Adam-Elmi <Adamcade123@gmail.com>
2025-02-03 22:03:14 +0300

    Updated Education section

--- src/Sections/Education.jsx
+++ src/Sections/Education.jsx
@@ -12,18 +12,18 @@ export default function Eduction({ education, defaultValue }) {
         {values && Array.isArray(values)
           ? values.map((v, i) => (
               <div key={i}>
-                <div id={"degree-" + (i + 1) + "wrapper"}>
-                  <p id={"degree-" + (i + 1)}>{v.degree}</p>
-                </div>
-                <div id={"institution-" + (i + 1) + "wrapper"}>
-                  <p id={"institution-" + (i + 1)}>{v.institution}</p>
-                </div>
-                <div id={"ed-year-" + (i + 1) + "wrapper"}>
-                  <p id={"ed-year-" + (i + 1)}>{v.year}</p>
-                </div>
-                <div id={"ed-details-" + (i + 1) + "wrapper"}>
-                  <p id={"ed-details-" + (i + 1)}>{v.details}</p>
-                </div>
+                <ul id={"institution-" + (i + 1) + "wrapper"}>
+                  <li id={"institution-" + (i + 1)}>{v.institution}</li>
+                </ul>
+                <ul id={"ed-year-" + (i + 1) + "wrapper"}>
+                  <li id={"ed-year-" + (i + 1)}>{v.year}</li>
+                </ul>
+                <ul id={"degree-" + (i + 1) + "wrapper"}>
+                  <li id={"degree-" + (i + 1)}>{v.degree}</li>
+                </ul>
+                <ul id={"ed-details-" + (i + 1) + "wrapper"}>
+                  <li id={"ed-details-" + (i + 1)}>{v.details}</li>
+                </ul>
               </div>
             ))
           : ""}
