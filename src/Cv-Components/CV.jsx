e1f9fee033fd491500b2fb7f1d55c2a7ba859035
Adam-Elmi <Adamcade123@gmail.com>
2025-02-03 22:03:09 +0300

    Updated CV component

--- src/Cv-Components/CV.jsx
+++ src/Cv-Components/CV.jsx
@@ -5,13 +5,14 @@ import Template from "./Template";
 export default function CV() {
   return (
     <div className=" w-full flex flex-col justify-center items-center">
-      <div className="w-full min-h-[70px] bg-slate-900 flex items-center px-2">
-        <button className="text-white flex gap-2 justify-center items-center border-2 border-slate-600 p-2 rounded-md">
-          <span className="fa-solid fa-arrow-left text-slate-700"></span>
-          <span className="font-[500]">Templates</span>
-        </button>
-        <div className="flex-1 text-white text-center">
-          <input type="text" className="p-2 max-w-full w-[300px] text-center focus:text-left text-slate-300 bg-transparent outline-none focus:border-b-[1.5px] border-slate-700" placeholder="Enter the name of your CV (e.g., My-CV)." />
+      <div className="w-full min-h-[70px] bg-slate-900 flex justify-between items-center px-2">
+        <div className="flex gap-5">
+          <button title="Home" className="text-white flex gap-2 justify-center items-center p-2 rounded-md">
+            <span className="fa-solid fa-home text-slate-700 text-[1.2rem]"></span>
+          </button>
+          <button title="Templates" className="text-white flex gap-2 justify-center items-center p-2 rounded-md">
+            <span className="fa-solid fa-file text-slate-700 text-[1.2rem]"></span>
+          </button>
         </div>
         <button className="text-white bg-indigo-600 flex gap-2 justify-center items-center border-2 border-slate-700 p-2 rounded-md">
           <span className="font-[500]">Download</span>
