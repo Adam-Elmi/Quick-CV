85121783c417ad45e7717173801f42e2fe1d1cb4
Adam-Elmi <Adamcade123@gmail.com>
2025-05-06 18:18:26 +0300

    Refined React-pdf export logic

--- src/Cv-Components/React-pdf.jsx
+++ src/Cv-Components/React-pdf.jsx
@@ -1,38 +1,47 @@
-import { PDFViewer, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer'
+import {
+  PDFViewer,
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+} from "@react-pdf/renderer";
 
 // Create styles
 const styles = StyleSheet.create({
-    page: {
-        flexDirection: 'row',
-        backgroundColor: '#fff',
-    },
-    section: {
-        margin: 10,
-        padding: 10,
-        flexGrow: 1
-    },
-    text: {
-        color: "red"
-    },
-    viewer: {
-        width: '100%', 
-        height: '100vh',
-    }
+  page: {
+    // flexDirection: "row",
+    backgroundColor: "#fff",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+    backgroundColor: "red"
+  },
+  text: {
+    color: "red",
+  },
+  viewer: {
+    width: "100%",
+    height: "100vh",
+  },
 });
 
 const MyDocument = () => (
- <PDFViewer style={styles.viewer}>
-         <Document >
-        <Page width={100} size="A4" style={styles.page}>
-            <View style={styles.section}>
-                <Text>Template 101</Text>
-            </View>
-            <View style={styles.section}>
-                <Text>Adam</Text>
-            </View>
-        </Page>
+  <PDFViewer style={styles.viewer}>
+    <Document>
+      <Page width={100} size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text style={styles.text}>Template 101</Text>
+        </View>
+        <View style={styles.section}>
+          <Text id="text">Adam</Text>
+          <Text>Elmi</Text>
+        </View>
+      </Page>
     </Document>
- </PDFViewer>
+  </PDFViewer>
 );
 
 export default MyDocument;
