4c81f104ac9cce7106d8b5de5e5ed339d405ab63
Adam-Elmi <Adamcade123@gmail.com>
2025-05-27 10:31:23 +0300

    Updated Temp-Section.jsx

--- src/Site-Components/Temp-Section.jsx
+++ src/Site-Components/Temp-Section.jsx
@@ -1,7 +1,12 @@
 export default function TempSection() {
-    return (
-        <div className="min-h-screen">
-            <h1>Templates</h1>
+  return (
+    <div className="min-h-screen p-2">
+      <h1>Templates</h1>
+      <div className="w-full flex justify-center items-center">
+        <div className="w-[800px] min-w-[350px] h-[800px] border-2 border-slate-200 shadow-xl max-[880px]:scale-80 max-[880px]:transform">
+          Adam Elmi
         </div>
-    )
-};
+      </div>
+    </div>
+  );
+}
