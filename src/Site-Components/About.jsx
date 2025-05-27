7c75bb163e67ad58e6191d7f84a75c1985a0f026
Adam-Elmi <Adamcade123@gmail.com>
2025-05-27 10:31:13 +0300

    Updated About.jsx

--- src/Site-Components/About.jsx
+++ src/Site-Components/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
         <h1 className="text-4xl text-center mt-10">
           About <span className="paul text-indigo-500">Quick-CV</span>
         </h1>
-        <p className="text-center text-slate-500 text-lg leading-9 mt-5 p-2 mx-2 bg-white border shadow-md w-fit rounded-md">
+        <p className="text-center text-slate-500 text-lg leading-9 mt-5 p-2 mx-2 bg-white border border-slate-100 shadow-md w-fit rounded-md">
           <span className="fa-solid fa-cubes text-indigo-500 mr-2"></span>
           This is a project that I created to help people create their CVs easily.
           The project is open-source and you can contribute to it by visiting
@@ -22,7 +22,7 @@ export default function About() {
         {aboutQuickCv.map((part, id) => (
           <div
             key={id}
-            className="border mx-5 bg-white shadow-md rounded-md my-3 w-[1100px] max-w-full"
+            className="border border-slate-100 mx-5 bg-white shadow-md rounded-md my-3 w-[1100px] max-w-full"
           >
             <div className="border-b border-slate-200 h-[35px] p-2 flex items-center">
               <span className={`p-2 font-[500] ${part.title === 'Intro' ? 'text-blue-500' : part.title === 'Problems' ? 'text-red-500' : part.title === 'Solution' ? 'text-green-500' : 'text-purple-500'}`}>
