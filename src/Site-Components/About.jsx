5223ea959e1e7a9e5219842b1f36c6128024488d
Adam-Elmi <Adamcade123@gmail.com>
2025-06-23 13:32:18 +0300

    Update About.jsx

--- src/Site-Components/About.jsx
+++ src/Site-Components/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
   );
   return (
     <div className="min-h-screen bg-gray-100">
-      <img style="width: 500px" src="src/assets/media/naruto_togather.jpg"/>
+      <img src="src/assets/media/naruto_togather.jpg"/>
       <div className="w-full flex justify-center items-center flex-col gap-5 px-2">
         <h1 className="text-4xl text-center mt-10">
           About <span className="paul text-indigo-500">Quick-CV</span>
