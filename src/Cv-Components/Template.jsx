4949c48a28f397205e732e284c28ef81e544f6a6
Adam-Elmi <Adamcade123@gmail.com>
2025-06-23 13:09:25 +0300

    Update Template.jsx

--- src/Cv-Components/Template.jsx
+++ src/Cv-Components/Template.jsx
@@ -2,8 +2,15 @@ import { PDFViewer } from "@react-pdf/renderer";
 import MyDocument from "./React-pdf";
 export default function Template() {
   return (
-    <PDFViewer width="100%" height="600">
-      <MyDocument />
-    </PDFViewer>
+    <>
+      {/* <PDFDownloadLink document={<MyDocument />} fileName="document.pdf">
+        {({ loading }) =>
+          loading ? "Loading document..." : <button>Download</button>
+        }
+      </PDFDownloadLink> */}
+      <PDFViewer width="100%" height="600">
+        <MyDocument />
+      </PDFViewer>
+    </>
   );
 }
