8412b5d35707294f7d5db781ce8ed22a2d31b5df
Adam-Elmi <Adamcade123@gmail.com>
2025-06-28 00:32:22 +0300

    Update Workflow.jsx

--- src/Helper-Components/Workflow.jsx
+++ src/Helper-Components/Workflow.jsx
@@ -58,7 +58,7 @@ export default function Workflow({ sections }) {
           <button
             onClick={moveToClickedBtn}
             key={index}
-            className={`w-[20px] h-[20px] rounded-full ${currentIndex === index
+            className={`w-[20px] h-[20px] cursor-pointer rounded-full ${currentIndex === index
                 ? "text-white bg-blue-500"
                 : "text-blue-500 bg-white"
               } border-[1.5px] border-blue-500 text-center flex justify-center items-center`}
