596ded7f027676041f6e8268d3f40d7ee0dfdc64
Adam-Elmi <Adamcade123@gmail.com>
2025-06-28 00:32:25 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-
+import { SharedProvider } from "./Shared/SharedContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <SharedProvider>
+        <App />
+      </SharedProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
