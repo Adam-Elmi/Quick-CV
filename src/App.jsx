50fee7ce9f4354af0b7a142ba9be31fbd383fb97
Adam-Elmi <Adamcade123@gmail.com>
2025-06-28 00:32:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./Site-Components/Layout";
 import About from "./Site-Components/About";
 import Contributors from "./Site-Components/contributors";
 import TempSection from "./Site-Components/Temp-Section";
+import GenerateCv from "./Helper-Components/GenerateCv"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contributors" element={<Contributors />} />
         <Route path="/templates" element={<TempSection />} />
+        <Route path="/card" element={<GenerateCv/>} />
       </Route>
       <Route path="/create-cv" element={<CV />} />
     </Routes>
