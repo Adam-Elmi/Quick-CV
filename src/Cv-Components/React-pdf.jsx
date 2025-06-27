684152de1ab3b55fa3f06f0dddd644d88b9ddf9f
Adam-Elmi <Adamcade123@gmail.com>
2025-06-28 00:32:16 +0300

    Update React-pdf.jsx

--- src/Cv-Components/React-pdf.jsx
+++ src/Cv-Components/React-pdf.jsx
@@ -1,24 +1,20 @@
+import PropTypes from "prop-types";
 import { Document, Page } from "@react-pdf/renderer";
-import imageSrc from "../assets/media/naruto_togather.jpg";
 import HTML from "react-pdf-html";
-const color = "red";
-const htmlContent = `
-  <h1 style="color: ${color}; border: 2px dotted red">Adam Elmi</h1>
-  <p>Self-taught developer from Somaliland</p>
-  <img style="width: 500px" src=${imageSrc}/>
-  <ul>
-    <li style="color: green;">SomCheat</li>
-    <li>SomBot</li>
-    <li>SomGlossary</li>
-  </ul>
-`;
+import basic_cv from "../Templates/basic-cv/basic_cv";
 
-const MyDocument = () => (
-  <Document>
-    <Page size="A4">
-      <HTML>{htmlContent}</HTML>
-    </Page>
-  </Document>
-);
 
-export default MyDocument;
+export default function MyDocument({ inputValue }) {
+  const name = inputValue?.trim() || "Adam Elmi";
+  return (
+    <Document>
+      <Page size="A4">
+        <HTML>{basic_cv(name)}</HTML>
+      </Page>
+    </Document>
+  );
+}
+
+MyDocument.propTypes = {
+  inputValue: PropTypes.string
+}
