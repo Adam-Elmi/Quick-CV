d7e557fd435a306eb5bad4918bb693455341a890
Adam-Elmi <Adamcade123@gmail.com>
2025-08-25 02:54:11 +0300

    Create React-pdf.tsx

---
+++ src/Cv-Components/React-pdf.tsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import { Document, Page } from "@react-pdf/renderer";
+import HTML from "react-pdf-html";
+import basic_cv from "../Templates/basic-cv/basic_cv";
+
+
+export default function MyDocument({ inputValue } : {inputValue: string}) {
+  const name = inputValue?.trim() || "Adam Elmi";
+  return (
+    <Document>
+      <Page size="A4">
+        <HTML>{basic_cv(name)}</HTML>
+      </Page>
+    </Document>
+  );
+}
+
+MyDocument.propTypes = {
+  inputValue: PropTypes.string
+}
