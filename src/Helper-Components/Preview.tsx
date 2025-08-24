823aaf48bb40b65d089b9258a0d70265f3226803
Adam-Elmi <Adamcade123@gmail.com>
2025-08-25 02:54:29 +0300

    Create Preview.tsx

---
+++ src/Helper-Components/Preview.tsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import { useEffect } from "react";
+import { useRef } from "react";
+
+export default function Preview({ content }) {
+  const contentRef = useRef(null);
+  useEffect(() => {
+    if (contentRef.current) {
+      contentRef.current.innerHTML = content;
+    }
+  }, [content]);
+  return (
+    <div className="min-h-screen p-2">
+      <h1>Templates</h1>
+      <div className="w-full flex justify-center items-center">
+        <div
+          ref={contentRef}
+          className="w-[595px]  h-[842px] border-2 border-slate-200 shadow-xl max-[880px]:scale-80 max-[880px]:transform"
+        ></div>
+      </div>
+    </div>
+  );
+}
+
+Preview.propTypes = {
+  content: PropTypes.any,
+};
