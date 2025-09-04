512b12b59e665ab4f22b0de824e85c8d17026c79
Adam-Elmi <Adamcade123@gmail.com>
2025-09-05 01:19:34 +0300

    Cleaned up

--- src/Cv-Components/Template.tsx
+++ src/Cv-Components/Template.tsx
@@ -1,11 +1,9 @@
 import Preview from "../Helper-Components/Preview";
 import basic_cv from "../Templates/basic-cv/basic_cv";
-import { useShared } from "../Shared/SharedContext";
 export default function Template() {
-  const { inputValue } = useShared();
   return (
     <>
-      <Preview content={basic_cv(inputValue || "Adam Elmi")}/>
+      <Preview content={basic_cv("Adam Elmi")}/>
     </>
   );
 }
