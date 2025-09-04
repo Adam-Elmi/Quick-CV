e0adc94ee558039191779c3053362e47afb135da
Adam-Elmi <Adamcade123@gmail.com>
2025-09-05 01:20:28 +0300

    Cleaned up

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { SharedProvider } from "./Shared/SharedContext";
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
-      <SharedProvider>
         <App />
-      </SharedProvider>
     </BrowserRouter>
   </StrictMode>,
 );
