081d85c0e4a81f29e4a773bb72ede7309a8f12f9
Adam-Elmi <Adamcade123@gmail.com>
2025-09-27 20:31:50 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-createRoot(document.getElementById("root") as HTMLElement).render(
+import { BrowserRouter } from "react-router";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
         <App />
