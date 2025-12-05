e3bb377e72dbf78a7b6f72f538136154f4825257
Adam-Elmi <Adamcade123@gmail.com>
2025-12-05 05:49:17 +0300

    Update Template.tsx

--- src/Cv-Components/Template.tsx
+++ src/Cv-Components/Template.tsx
@@ -1,9 +1,38 @@
-import Preview from "../Helper-Components/Preview";
-import basic_cv from "../Templates/basic-cv/basic_cv";
-export default function Template() {
+import { BlobProvider } from "@react-pdf/renderer";
+import MyDocument from "./MyDocument";
+
+function Template() {
   return (
-    <>
-      <Preview content={basic_cv("Adam Elmi")}/>
-    </>
+    <div className="w-[595px] max-lg:w-full h-screen p-3">
+      <BlobProvider document={<MyDocument />}>
+        {({ url, loading }) => {
+          if (loading) return <span>Generating PDF…</span>;
+
+          return (
+            <div>
+              <button
+                className="bg-blue-500 p-2 text-white cursor-pointer hover:text-blue-200 rounded-lg"
+                onClick={() => {
+                  if (url) window.open(url, "_blank", "noopener,noreferrer");
+                }}
+              >
+                Preview PDF
+              </button>
+
+              <div style={{ marginTop: 20, height: "100vh" }}>
+                <embed
+                  src={url ? url : undefined}
+                  type="application/pdf"
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+            </div>
+          );
+        }}
+      </BlobProvider>
+    </div>
   );
 }
+
+export default Template;
