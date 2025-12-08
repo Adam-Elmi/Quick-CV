846bed5fbf8866060c2a25833bb3f4aa05bbfbc2
Adam-Elmi <Adamcade123@gmail.com>
2025-12-08 18:23:03 +0300

    Update Profile.tsx

--- src/Form-Components/Profile.tsx
+++ src/Form-Components/Profile.tsx
@@ -1,5 +1,5 @@
-import InputField from "./InputField";
-
+import InputWrapper from "./InputWrapper";
+// import ids from "../Utilities/ids";
 export default function Profile() {
   return (
     <div className="flex flex-col gap-8 border-2 border-slate-100 rounded-md bg-white p-2 m-4">
@@ -9,74 +9,74 @@ export default function Profile() {
       <form className="flex flex-col gap-4 flex-wrap justify-center">
         <div className="flex gap-4 flex-wrap justify-center">
           {/* First Name */}
-          <InputField
+          <InputWrapper
+            dataId="First-Name"
             type="text"
             label="First Name"
-            is_required={true}
             placeholder="Enter your first name"
             icon="fa-solid fa-user"
           />
           {/* Last Name */}
-          <InputField
+          <InputWrapper
+            dataId="Last-Name"
             type="text"
             label="Last Name"
-            is_required={true}
             placeholder="Enter your last name"
             icon="fa-solid fa-user"
           />
           {/* Title */}
-          <InputField
+          <InputWrapper
+          dataId = "Title"
             type="text"
             label="Title"
-            is_required={false}
             placeholder="Enter your title"
             icon="fa-solid fa-star"
           />
           {/* Email Address */}
-          <InputField
+          <InputWrapper
+          dataId = "Email"
             type="text"
             label="Email Address"
-            is_required={true}
             placeholder="Enter your email address"
             icon="fa-solid fa-envelope"
           />
           {/* Phone Number */}
-          <InputField
+          <InputWrapper
+          dataId = "Phone-Number"
             type="text"
             label="Phone Number"
-            is_required={true}
             placeholder="Enter your phone number"
             icon="fa-solid fa-phone"
           />
           {/* Website */}
-          <InputField
+          <InputWrapper
+          dataId = "Website"
             type="text"
             label="Website"
-            is_required={false}
             placeholder="Enter your website link"
             icon="fa-solid fa-link"
           />
           {/* LinkedIn */}
-          <InputField
+          <InputWrapper
+          dataId = "LinkedIN"
             type="text"
             label="LinkedIn"
-            is_required={false}
             placeholder="Enter your LinkedIn link"
             icon="fa-brands fa-linkedin"
           />
           {/* Country */}
-          <InputField
+          <InputWrapper
+          dataId = "Country"
             type="text"
             label="Country"
-            is_required={false}
             placeholder="Enter your country"
             icon="fa-solid fa-earth-americas"
           />
           {/* City */}
-          <InputField
+          <InputWrapper
+            dataId = "City"
             type="text"
             label="City"
-            is_required={false}
             placeholder="Enter your city"
             icon="fa-solid fa-city"
           />
