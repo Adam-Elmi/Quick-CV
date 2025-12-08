9c0651e579157a4aa9138d94eef3960d9a4c7265
Adam-Elmi <Adamcade123@gmail.com>
2025-12-08 18:22:59 +0300

    Update InputField.tsx

--- src/Form-Components/InputField.tsx
+++ src/Form-Components/InputField.tsx
@@ -1,21 +1,23 @@
-import React from "react";
-type InputFieldType = {
-  label: string;
-  type: string;
-  is_required: boolean;
-  placeholder: string;
-  icon: React.ReactNode;
-}
-export default function InputField({ label, type, is_required, placeholder, icon }: InputFieldType) {
+import type InputFieldType from "../types/inputTypes";
+export default function InputField({
+  label,
+  type,
+  placeholder,
+  icon,
+  dataId,
+  onChange,
+  input_value
+}: InputFieldType) {
   return (
     <div id="input-field" className="flex flex-col gap-1">
-      <label className="text-slate-500 bold flex items-center gap-2">
+      <label className="text-slate-500 bold flex items-center gap-2  max-w-full">
         <span className={icon + " text-sm text-gray-300"}></span>
         {label}
-        {is_required ? <span className="text-red-400">*</span> : null}
       </label>
       <input
-        required={is_required}
+      value={input_value}
+        onChange={onChange}
+        data-id={dataId}
         type={type}
         className="border border-gray-300 text-blue-700 placeholder:text-gray-400 py-2 px-3 mb-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
         placeholder={placeholder}
