e79163e60bfcc41ad47fe2a5d1a132da1351eed1
Adam-Elmi <Adamcade123@gmail.com>
2025-12-08 18:23:18 +0300

    Create ids.ts

---
+++ src/Utilities/ids.ts
@@ -0,0 +1,16 @@
+const ids = {
+  firstName: "Cv-First-Name",
+  lastName: "Cv-Last-Name",
+  title: "Cv-Title",
+  email: "Cv-Email",
+  phone: "CV-Phone-Number",
+  country: "Cv-Country",
+  city: "Cv-City",
+  education: "Cv-Education",
+  experience: "Cv-Experience",
+  skill: "Cv-Skill",
+  project: "Cv-Projects",
+  language: "CV-Languages",
+  award: "Cv-Awards"
+};
+export default ids;
