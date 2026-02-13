b4f07be958ec8cf1229e8c625a7039c0308c015e
Adam-Elmi <Adamcade123@gmail.com>
2026-02-13 16:22:49 +0300

    Update Profile.tsx

--- src/Form-Components/Profile.tsx
+++ src/Form-Components/Profile.tsx
@@ -1,5 +1,5 @@
 import InputWrapper from "./InputWrapper";
-// import ids from "../Utilities/ids";
+import DynamicInputGroup from "./DynamicInputGroup";
 export default function Profile() {
   return (
     <div className="flex flex-col gap-8 border-2 border-slate-100 rounded-md bg-white p-2 m-4">
@@ -7,8 +7,8 @@ export default function Profile() {
         Profile Summary
       </h1>
       <form className="flex flex-col gap-4 flex-wrap justify-center">
-        <div className="flex gap-4 flex-wrap justify-center">
-          {/* First Name */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-4xl px-4 justify-items-center">
+          {}
           <InputWrapper
             dataId="First-Name"
             type="text"
@@ -16,7 +16,7 @@ export default function Profile() {
             placeholder="Enter your first name"
             icon="fa-solid fa-user"
           />
-          {/* Last Name */}
+          {}
           <InputWrapper
             dataId="Last-Name"
             type="text"
@@ -24,62 +24,66 @@ export default function Profile() {
             placeholder="Enter your last name"
             icon="fa-solid fa-user"
           />
-          {/* Title */}
+          {}
           <InputWrapper
-          dataId = "Title"
+            dataId="Title"
             type="text"
             label="Title"
             placeholder="Enter your title"
             icon="fa-solid fa-star"
+            className="sm:col-span-2 sm:max-w-none"
           />
-          {/* Email Address */}
+          {}
           <InputWrapper
-          dataId = "Email"
+            dataId="Email"
             type="text"
             label="Email Address"
             placeholder="Enter your email address"
             icon="fa-solid fa-envelope"
           />
-          {/* Phone Number */}
+          {}
           <InputWrapper
-          dataId = "Phone-Number"
+            dataId="Phone-Number"
             type="text"
             label="Phone Number"
             placeholder="Enter your phone number"
             icon="fa-solid fa-phone"
           />
-          {/* Website */}
+          {}
           <InputWrapper
-          dataId = "Website"
-            type="text"
-            label="Website"
-            placeholder="Enter your website link"
-            icon="fa-solid fa-link"
-          />
-          {/* LinkedIn */}
-          <InputWrapper
-          dataId = "LinkedIN"
-            type="text"
-            label="LinkedIn"
-            placeholder="Enter your LinkedIn link"
-            icon="fa-brands fa-linkedin"
-          />
-          {/* Country */}
-          <InputWrapper
-          dataId = "Country"
+            dataId="Country"
             type="text"
             label="Country"
             placeholder="Enter your country"
             icon="fa-solid fa-earth-americas"
           />
-          {/* City */}
           <InputWrapper
-            dataId = "City"
+            dataId="City"
             type="text"
             label="City"
             placeholder="Enter your city"
             icon="fa-solid fa-city"
           />
+          {}
+          <div className="w-full max-w-4xl sm:col-span-2 sm:w-full">
+            <DynamicInputGroup
+              baseId="Profile_Link"
+              title="Social Links"
+              buttonLabel="Add Link"
+            >
+              {(id, index) => (
+                <InputWrapper
+                  key={id}
+                  dataId={id}
+                  type="text"
+                  label={`Link #${index + 1}`}
+                  placeholder="Enter a link..."
+                  icon="fa-solid fa-link"
+                  className="sm:max-w-none"
+                />
+              )}
+            </DynamicInputGroup>
+          </div>
         </div>
       </form>
     </div>
