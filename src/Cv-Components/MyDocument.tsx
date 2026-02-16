019a695bcc63a17eb68c0fabe32017d6435fe9e1
Adam-Elmi <Adamcade123@gmail.com>
2026-02-16 20:26:47 +0000

    Update MyDocument.tsx

--- src/Cv-Components/MyDocument.tsx
+++ src/Cv-Components/MyDocument.tsx
@@ -1,18 +1,25 @@
 import { Document, Page, Font } from "@react-pdf/renderer";
 import HTML from "react-pdf-html";
+import { useCVData } from "../Utilities/cvDataStore";
+import { TEMPLATES } from "../Templates";
+import { renderTemplate } from "../Utilities/placeholderRenderer";
+
 Font.register({
   family: "Roboto",
   src: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
 });
-import { useCVData } from "../Utilities/cvDataStore";
-import { TEMPLATES } from "../Templates";
+
 export default function MyDocument() {
   const cvData = useCVData();
   const activeTemplate = TEMPLATES.find(t => t.id === cvData.selectedTemplate) || TEMPLATES[0];
+
+  const rawHtml = activeTemplate.builder(cvData);
+  const renderedHtml = renderTemplate(rawHtml, cvData);
+
   return (
     <Document>
       <Page size="A4" style={{ padding: 30 }}>
-        <HTML>{activeTemplate.builder(cvData)}</HTML>
+        <HTML>{renderedHtml}</HTML>
       </Page>
     </Document>
   );
